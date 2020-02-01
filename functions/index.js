c50ab5e25e99462485283c6fe1ad4c414ecffc13
jasonTelanoff <jason.telanoff@gmail.com>
2020-02-01 01:16:47 -0800

    Added a machine learning algorithm
    Added a function that comapares a photo given as a parameter to Google cloud's database. It will then print all the information about it.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,36 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+// Imports the Google Cloud client libraries
+const vision = require('@google-cloud/vision');
+const fs = require('fs');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+exports.onUpload = functions.storage.object().onFinalize(async (object) => {
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    const fileName = object.name;
+    const request = {
+      image: {content: fs.readFileSync(fileName)},
+    };
+
+    const [result] = await client.objectLocalization(request);
+    const objects = result.localizedObjectAnnotations;
+    objects.forEach(object => {
+      if(object.name === `Packaged goods`) {
+        console.log(`yes`);
+      } else {
+        console.log(`${object.name}`);
+      }
+      console.log(`Confidence: ${object.score}`);
+      const vertices = object.boundingPoly.normalizedVertices;
+      vertices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
+    });
+  });
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
