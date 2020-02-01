faa3cec6a7c06285c169154b6cbe24b6537b2aa9
jasonTelanoff <jason.telanoff@gmail.com>
2020-02-01 13:24:12 -0800

    Started functions

--- functions/index.js
+++ functions/index.js
@@ -22,7 +22,23 @@ exports.onUpload = functions.storage.object().onFinalize(async (object) => {
       const veritices = object.boundingPoly.normalizedVertices;
       veritices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
     });
-  });
+});
+
+exports.signUpNewUser = functions.auth.user().onCreate((user) => {
+
+});
+
+exports.joinEvent = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const event = req.body.eventID;
+  const userToken = req.headers.authorization;
+
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.database().ref('/messages').push({original: original});
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
