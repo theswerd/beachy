74a6d86565ed567f943363f50d64c84e2826dc82
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-01-31 21:55:41 -0800

    Implimented Theme

--- beachy/lib/constants.dart
+++ beachy/lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'constants/colorscheme.dart';
 
 class Constants {
-  static AppColors lightColors = new AppColors(Colors.orange, Colors.yellow);
-  static AppColors darkColors = new AppColors(Colors.orange, Colors.yellow);
+  static AppColors lightColors = new AppColors(Color(0xff00a0dd), Color(0xfffc5130));
+  static AppColors darkColors = new AppColors(Color(0xff303036), Color(0xffe91d2d));
 
 }
