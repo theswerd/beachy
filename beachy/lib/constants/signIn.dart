1a5ed550b68ee610748814ae7267e6451ab5c9e6
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-02-01 01:28:50 -0800

    Added new logo

--- beachy/lib/constants/signIn.dart
+++ beachy/lib/constants/signIn.dart
@@ -2,16 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 Future<bool> signInWithGoogle()async{
-  GoogleSignIn _googleSignIn = GoogleSignIn(
-    scopes: [
-      'email',
-      'https://www.googleapis.com/auth/contacts.readonly',
-    ],
-  );
+  return true;
+  // GoogleSignIn _googleSignIn = GoogleSignIn(
+  //   scopes: [
+  //     'email',
+  //     'https://www.googleapis.com/auth/contacts.readonly',
+  //   ],
+  // );
   
-  GoogleSignInAccount account = await _googleSignIn.signIn();
+  // GoogleSignInAccount account = await _googleSignIn.signIn();
   
-  return account == null?false:true;
+  // return account == null?false:true;
 
 }
 
